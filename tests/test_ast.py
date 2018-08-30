68872b84d7d0dce28429e7b38cec4cec797a8daf
Skye M <skyem@hotmail.co.uk>
2018-08-30 16:34:52 +0100

    Converted AST tests to pytest format
    also removed the grammar tests for now

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,17 +1,17 @@
 from tests import *
 
-class AST(TestCase):
-    def test_lookup(self):
-        """ Lookup of python classes"""
-        self.assertIsInstance(node.Expression(), grammar.Expression)
-        #self.assertIsInstance(node.Add(), grammar.Operator)
+def test_lookup():
+    """ Lookup of python classes"""
+    assert isinstance(node.Expression(), grammar.Expression)
+    #assert isinstance(node.Add(), grammar.Operator)
 
-    def test_ast(self):
-        """ Equality """
-        self.assertEqual(node.String(" Node "), node.String(" Node "))
-        self.assertNotEqual(node.String(" Node "), node.String(" Yo "))
+def test_ast():
+    """ Equality """
+    assert node.String(" Node ") == node.String(" Node ")
+    assert node.String(" Node ") != node.String(" Yo ")
 
-    def test_ast_rec(self):
-        """ Equality, recursive """
-        self.assertEqual(prog(node.String(" Node ")), prog(node.String(" Node ")))
-        self.assertEqual(prog(node.String(" Node ")), prog(node.String(" Yo ")))
+def test_ast_rec():
+    """ Equality, recursive """
+    assert prog(node.String(" Node ")) == prog(node.String(" Node "))
+    assert prog(node.String(" Node ")) != prog(node.String(" Yo "))
+
