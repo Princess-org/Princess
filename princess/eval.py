23136a8317fcd0c18512e09756ce1cb6bbba08c1
Victorious3 <vic@nightfall.moe>
2018-10-01 20:19:23 +0200

    interpreter, stack

--- princess/eval.py
+++ princess/eval.py
@@ -1,47 +1,130 @@
-import operator
+import operator, os
+from ctypes import *
 from tatsu.model import NodeWalker
 from enum import Enum
-from .ast import *
+from . import ast
 
-class TType: pass
-class TNumeric(TType): pass
+if os.name == "nt":
+    libc = cdll.LoadLibrary("msvcrt.dll")
+else:
+    libc = cdll.LoadLibrary("libc.so.6")
 
-class TFloat(TNumeric):
+libc.calloc.restype = c_void_p
+libc.malloc.restype = c_void_p
+
+class StackOverflow(Exception): pass
+class StackUnderflow(Exception): pass
+
+class Stack:
     def __init__(self, size):
-        self.bitsize = size
+        self.ptr = 0
+        self.size = size
+        self.buffer = libc.calloc(self.size, sizeof(c_byte))
+        self.frames = []
 
-class TInt(TNumeric):
-    def __init__(self, size):
-        self.bitsize = size
+    def __del__(self):
+        libc.free(self.buffer)
 
-class TUint(TNumeric):
-    def __init__(self, size):
-        self.bitsize = size
+    def allocate(self, n):  # Allocates raw memory
+        if self.ptr + n > self.size:
+            raise StackOverflow()
+        ptr = self.ptr
+        self.ptr += n
+        return ptr
 
-class TStruct(TType):
-    def __init__(self, name, attributes):
-        self.name = name
-        self.attributes = attributes
+    def free(self, n):  # Frees stack space
+        if self.ptr - n < 0:
+            raise StackUnderflow()
+        self.ptr -= n
 
-class TypedValue:
-    def __init__(self, type: TType, value):
-        self.type = type
-        self.value = value
+    def push(self, value):  # Returns a pointer to the value on the stack
 
-    def __getattribute__(self, attr):
-        return getattr(self.expr, attr)
+        if self.ptr + sizeof(value) > self.size:
+            raise StackOverflow()
+        slot = cast(self.buffer + self.ptr, POINTER(type(value)))
+        slot[0] = value
+        self.ptr += sizeof(value)
 
-def binop(left: TypedValue, right: TypedValue, operator):
-    result = operator(left.value, right.value)
+        return slot
 
+    def pop(self, tpe):
+        value = self.peek(tpe, sizeof(tpe)).contents
+        self.ptr -= sizeof(tpe)
+        return value
+        
+    def peek(self, tpe, offset = 0): # Returns a pointer
+        offset = self.ptr - offset
+        if offset < 0:
+            raise StackUnderflow()
+
+        slot = cast(self.buffer + offset, POINTER(tpe))
+        return slot
+
+class Environment:
+    def __init__(self, stack: Stack):
+        self.stack = stack
+        self.stack_frames = []
+    
+    def push(self, value):
+        return self.stack.push(value)
+    
+    def pop(self, tpe):
+        return self.stack.pop(tpe)
+
+    @property
+    def frame(self):
+        return self.stack_frames[-1]
+
+    def get_local(self, i):
+        Frame = self.frame
+        tpe = Frame.types[i]
+        field = Frame.field(i)
+        return self.stack.peek(tpe, sizeof(Frame) - field.offset).contents
+
+    def set_local(self, i, value):
+        Frame = self.frame
+        field = Frame.field(i)
+        self.stack.peek(type(value), sizeof(Frame) - field.offset).contents = value
+
+    def get_env(self, i):
+        Frame = self.frame
+        Frame.env[i].contents
+    
+    def set_env(self, i, value):
+        Frame = self.frame
+        Frame.env[i].contents = value
+
+    def push_frame(self, *args, env = []):  # args is a list of types, env is a list of pointers
+
+        class Frame(Structure):
+            _fields_ = [(str(i), v) for (i, v) in enumerate(args)]
+            types = args
+            env = env
+
+            @classmethod
+            def field(cls, i):
+                return getattr(cls, str(i))
+
+        self.stack_frames.append(Frame)
+        self.stack.allocate(sizeof(Frame))
+
+    def pop_frame(self):
+        Frame = self.stack_frames.pop()
+        self.stack.free(sizeof(Frame))
+
+def binop(left, right, operator):
+    return operator(left.value, right.value)
 
 class Eval(NodeWalker):
+
+    def __init__(self):
+        self.env = Environment(Stack(128 * 1000)) # 128kb of stack size
     
-    def walk_Add(self, node: Add):
+    def walk_Add(self, node: ast.Add):
         return binop(self.walk(node.left), self.walk(node.right), operator.add)
     
-    def walk_Mul(self, node: Mul):
+    def walk_Mul(self, node: ast.Mul):
         return binop(self.walk(node.left), self.walk(node.right), operator.mul)
 
-    def walk_Div(self, node: Div):
+    def walk_Div(self, node: ast.Div):
         return binop(self.walk(node.left), self.walk(node.right), operator.truediv)
---
+++ tests/interpreter/test_stack.py
@@ -0,0 +1,47 @@
+import sys
+from ctypes import *
+from pytest import raises
+
+from princess.eval import Stack, Environment, StackOverflow, StackUnderflow
+
+
+def test_basic():
+    stack = Stack(sizeof(c_int) * 2) # Stack for two integers
+
+    stack.push(c_int(42))
+    stack.push(c_int(-10))
+
+    with raises(StackOverflow):
+        stack.push(c_int(10))
+
+    assert stack.pop(c_int).value == -10
+    assert stack.pop(c_int).value == 42
+
+    with raises(StackUnderflow):
+        stack.pop(c_int)
+
+def test_type_conv():
+    stack = Stack(sizeof(c_uint16))
+    stack.push(c_uint16(0b1010101011111111))
+
+    assert stack.pop(c_uint8).value == (0b10101010 if sys.byteorder == 'little' else 0b11111111)
+    assert stack.pop(c_uint8).value == (0b11111111 if sys.byteorder == 'little' else 0b10101010)
+
+    assert stack.ptr == 0
+
+def test_stack_frame():
+    env = Environment(Stack(128 * 1000))
+    env.push_frame(c_int, c_bool, c_float)
+    env.set_local(0, c_int(20))
+    env.set_local(1, c_bool(True))
+    env.set_local(2, c_float(12.42))
+
+    env.push_frame(c_byte * 20, c_double)
+    env.pop_frame()
+
+    assert env.get_local(0).value == 20
+    assert env.get_local(1).value == True
+    assert env.get_local(2).value == c_float(12.42).value
+
+    env.pop_frame()
+    assert env.stack.ptr == 0
