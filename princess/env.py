c212bc68059d2f24f74058c7d82399c1355b99fb
Victorious3 <vic@nightfall.moe>
2020-01-03 16:47:05 +0100

    Fix PEnum

--- princess/env.py
+++ princess/env.py
@@ -64,7 +64,7 @@ class PEnum(object):
 		return self.__dict__.__setitem__(name, val)
 
 	def __delitem__(self, name):
-		if self.__dict__.has_key(name):
+		if name in self.__dict__:
 			del self.__dict__[name]
 
 	def __getattr__(self, name):
--- tests/compiler/test_imp.py
+++ tests/compiler/test_imp.py
@@ -1,6 +1,6 @@
 # This file was compiled by the grace of your highness
 # ~ Princess Vi Nightfall ~
-# on 2020-01-03 15:36:43.350863
+# on 2020-01-03 15:57:11.759726
 #
 # Bow to the princess!
 
@@ -10,9 +10,9 @@ from princess.pbuiltins import *
 __env = Environment()
 
 # --- start of code ---
-def plus_one(_48a):
-    return p_return(((c_long(_48a.value + p_cast(int(1), c_long).value)),))
-def plus(_49a, _49b):
-    return p_return(((c_long(_49a.value + _49b.value)),))
+def plus_one(_3a):
+    return p_return(((c_long(_3a.value + p_cast(int(1), c_long).value)),))
+def plus(_4a, _4b):
+    return p_return(((c_long(_4a.value + _4b.value)),))
 foo = p_declare((int(20),), (c_long,))
 # --- end of code ---
