2ffb83eb4eee0536b4e221497a495d35a4addc18
Victorious3 <vic@nightfall.moe>
2021-06-22 18:46:07 +0200

    Update travis

--- travis.sh
+++ travis.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 set -e
 
+VERSION="0.0.1"
+SOURCE_URL="https://github.com/Princess-org/Princess/releases/download/v${VERSION}-alpha/princess-${VERSION}.zip"
+
+echo "Downloading Princess compiler from github..."
 mkdir -p bin build
-# TODO
+wget -O bin/princess-dl.zip $SOURCE_URL
+unzip -d bin/princess-dl bin/princess-dl.zip
+mv bin/princess-dl/bin/princess bin/princess
+rm -r bin/princess-dl
+rm bin/princess-dl.zip
+
+echo "Building the compiler..."
+./bin/princess -d --include=src --buildfolder=build --outfile=bin/princess2 src/main.pr
+echo "Running test suite..."
+./bin/princess2 --test
+echo "Compiling secondary test..."
+./bin/princess2 -d --include=src --buildfolder=build --outfile=bin/test src/test/test.pr
+./bin/test
