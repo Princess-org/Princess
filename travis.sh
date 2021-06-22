1742f0905e5233d9bd5cf003d0ca229d6fda7100
Victorious3 <vic@nightfall.moe>
2021-06-22 19:32:18 +0200

    chmod

--- travis.sh
+++ travis.sh
@@ -13,9 +13,12 @@ rm -r bin/princess-dl
 rm bin/princess-dl.zip
 
 echo "Building the compiler..."
+chmod +x ./bin/princess
 ./bin/princess -d --include=src --buildfolder=build --outfile=bin/princess2 src/main.pr
 echo "Running test suite..."
+chmod +x ./bin/princess2
 ./bin/princess2 --test
 echo "Compiling secondary test..."
 ./bin/princess2 -d --include=src --buildfolder=build --outfile=bin/test src/test/test.pr
+chmod +x ./bin/test
 ./bin/test
