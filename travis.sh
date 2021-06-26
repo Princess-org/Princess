0816de829fb49fba6341bde8a5d53d3688321aca
Victorious3 <vic@nightfall.moe>
2021-06-26 18:32:59 +0200

    Use a tar archive

--- travis.sh
+++ travis.sh
@@ -2,24 +2,20 @@
 set -e
 
 VERSION="0.0.1"
-SOURCE_URL="https://github.com/Princess-org/Princess/releases/download/v${VERSION}-alpha/princess-${VERSION}.zip"
+SOURCE_URL="https://github.com/Princess-org/Princess/releases/download/v${VERSION}-alpha/princess-${VERSION}.tar.gz"
 
 echo "Downloading Princess compiler from github..."
 mkdir -p bin build
-wget -O bin/princess-dl.zip $SOURCE_URL
-unzip -d bin/princess-dl bin/princess-dl.zip
+wget -O bin/princess-dl.tar.gz $SOURCE_URL
+mkdir bin/princess-dl && tar xf bin/princess-dl.tar.gz -C bin/princess-dl --strip-components 1
 mv bin/princess-dl/bin/princess bin/princess
 rm -r bin/princess-dl
-rm bin/princess-dl.zip
+rm bin/princess-dl.tar.gz
 
-# TODO Set the executable flag
 echo "Building the compiler..."
-chmod +x ./bin/princess
 ./bin/princess -d --include=src --buildfolder=build --outfile=bin/princess2 src/main.pr
 echo "Running test suite..."
-chmod +x ./bin/princess2
 ./bin/princess2 --test
 echo "Compiling secondary test..."
 ./bin/princess2 -d --include=src --buildfolder=build --outfile=bin/test src/test/test.pr
-chmod +x ./bin/test
 ./bin/test
