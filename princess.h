1ebbdae61ce15b80f14e83d6ed9f5f2bbfbb9b4e
Victorious3 <vic@nightfall.moe>
2021-09-30 17:44:38 +0200

    Remove stuff from princess.h

--- princess.h
+++ princess.h
@@ -1,12 +1,6 @@
 #ifndef PRINCESS_H
 #define PRINCESS_H
 
-#ifdef _WIN32
-#define DLL_EXPORT __declspec(dllexport)
-#else
-#define DLL_EXPORT
-#endif
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -31,36 +25,6 @@ extern FILE *stdin;
 #else
 #include <fileapi.h>
 #endif
-
-typedef FILE *File;
-
-typedef uint8_t     uint8;
-typedef int8_t      int8;
-typedef uint16_t    uint16;
-typedef int16_t     int16;
-typedef uint32_t    uint32;
-typedef int32_t     int32;
-typedef uint64_t    uint64;
-typedef int64_t     int64;
-
-typedef char        byte;
-typedef unsigned char   ubyte;
-typedef unsigned int    uint;
-typedef unsigned short  ushort;
-typedef unsigned long   ulong;
-typedef float   float32;
-typedef double  float64;
-
-typedef struct {
-    size_t size;
-    void *value;
-} Array;
-
-typedef Array string;
-
-#define ARRAY($name, $type, $size) \
-    $type _##$name[$size];         \
-    Array $name = {$size, _##$name}
 
 // For some reason we have to declare prototypes for 
 // clang to output stuff into the json files
