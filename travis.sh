226883f9c269d97b67381414518e5b9e6cfac09b
Victorious3 <vic@nightfall.moe>
2021-10-08 11:06:45 +0200

    Fix file path

--- travis.sh
+++ travis.sh
@@ -16,6 +16,6 @@ python3.9 include/gencstd.py
 echo "Building the compiler..."
 ./bin/princess -d --include=src --buildfolder=build --outfile=bin/princess2 src/main.pr
 echo "Building test suite..."
-./bin/princess2 -d --include=src --buildfolder=build --outfile=bin/testsuite src/tests/main.pr
+./bin/princess2 -d --include=src --buildfolder=build --outfile=bin/testsuite src/test/main.pr
 echo "Running test suite..."
 ./bin/testsuite
