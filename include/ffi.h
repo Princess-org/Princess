b60f02643fa4fd3df2e9bf4081885af574cf1fd7
Victorious3 <vic@nightfall.moe>
2023-01-04 22:01:05 +0100

    FFI

--- include/ffi.h
+++ include/ffi.h
@@ -1,6 +1,7 @@
 // This file needs to be added to the import path on windows
 /*
 %EXCLUDE ffi_raw_call ffi_ptrarray_to_raw ffi_raw_to_ptrarray ffi_raw_size ffi_java_raw_call ffi_java_ptrarray_to_raw ffi_java_raw_to_ptrarray ffi_java_raw_size ffi_prep_raw_closure ffi_prep_raw_closure_loc ffi_prep_java_raw_closure ffi_prep_java_raw_closure_loc
+%EXCLUDE ffi_prep_closure
 %EXCLUDE __va_start
 */
 #ifdef _WIN32
