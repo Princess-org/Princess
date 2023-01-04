647f302472883d8325fd9fa5ab467f7835bf38ff
Victorious3 <vic@nightfall.moe>
2023-01-04 22:00:22 +0100

    ignores

--- include/linux.h
+++ include/linux.h
@@ -1,7 +1,7 @@
 /*
 %EXCLUDE sigstack sigreturn siggetmask tmpnam tmpnam_r tempnam sys_errlist sys_nerr lchmod getwd revoke mktemp setlogin atof atoi atol strtod strtol strtoul calloc free malloc realloc abort atexit exit getenv system bsearch qsort abs div labs ldiv srand rand mblen mbstowcs mbtowc wcstombs wctomb alloca sys_siglist _sys_siglist
 %EXCLUDE shm_open crypt shm_unlink
-%EXCLUDE __builtin_bswap32 ___builtin_bswap64 _profil _unwhiteout
+%EXCLUDE __builtin_bswap32 __builtin_bswap64 profil unwhiteout UUID_NULL
 */
 
 #ifndef _WIN32
