9abd8dbd7947c165cd0b5bbdd3db667a0ddf1b05
Victorious3 <vic@nightfall.moe>
2023-01-04 17:33:51 +0100

    Remove unnecessary stuff

--- include/linux.h
+++ include/linux.h
@@ -1,6 +1,6 @@
 /*
 %EXCLUDE sigstack sigreturn siggetmask tmpnam tmpnam_r tempnam sys_errlist sys_nerr lchmod getwd revoke mktemp setlogin atof atoi atol strtod strtol strtoul calloc free malloc realloc abort atexit exit getenv system bsearch qsort abs div labs ldiv srand rand mblen mbstowcs mbtowc wcstombs wctomb alloca sys_siglist _sys_siglist
-
+%EXCLUDE shm_open crypt shm_unlink
 */
 
 #ifndef _WIN32
