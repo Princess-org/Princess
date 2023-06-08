671832e85f0606aea04e55c516ecb1e41e3a9892
Victorious3 <vic@nightfall.moe>
2023-06-08 21:35:46 +0200

    Fix size_t and anonymous structs being members multiple times

--- build.py
+++ build.py
@@ -131,8 +131,6 @@ def build(extra):
     args = [exe_file("bin/princess"), "-d", "--no-incremental", "-Isrc", "--buildfolder=build", "--outfile", exe_file("bin/princess2"), "src/main.pr"]
     if sys.platform == "win32":
         args += WIN_ARGS
-    else:
-        args.append("-lbfd")
         
     subprocess.check_call(args + extra)
 
